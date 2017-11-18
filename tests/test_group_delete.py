67a0e5da1d8c5117d9c3cef7790f029061fd1179
Natali2411 <nat.tiutiunnyk@gmail.com>
2017-11-18 13:32:23 +0200

    Address Book_HomeWork

--- tests/test_group_delete.py
+++ tests/test_group_delete.py
@@ -1,10 +1,13 @@
 import random
 
-
 def test_delete_group(app, login_admin):
     index = random.randrange(app.count_groups())
     app.open_group_page()
+    c1 = app.count_groups()
     app.delete_group_by_number(index)
-    # TODO: verification of message
+    # TODO: verification of message\
+    assert "Group has been removed." in app.wd.find_element_by_id("content").text
     app.return_to_group_page()
-    # TODO: Verification that group added
+    c2 = app.count_groups()
+    # TODO: Verification that group removed
+    assert c1 != c2
