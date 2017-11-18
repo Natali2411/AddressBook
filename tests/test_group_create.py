0ada1dbfa2964ddd8f40c658a01df7da7b8628d2
Natali2411 <nat.tiutiunnyk@gmail.com>
2017-11-18 13:32:47 +0200

    Address Book_HomeWork

--- tests/test_group_create.py
+++ tests/test_group_create.py
@@ -1,6 +1,9 @@
 def test_group_create(app, login_admin):
     app.open_group_page()
-    app.create_group(name="1234", header="12345", footer="1234")
+    app.create_group(name="group2", header="12345", footer="1234")
     # TODO: verification of message
+    assert "A new group has been entered into the address book." in app.wd.find_element_by_id("content").text
     app.return_to_group_page()
     # TODO: Verification that group added
+    assert "group2" in app.wd.find_element_by_id("content").text
+
